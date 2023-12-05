44b0e6bce0da11c198388ea1ace768979e26945e
auliarahman789 <mauliarahman214@gmail.com>
2023-12-05 11:59:15 +0700

    Initial

---
+++ src/app/components/introduction.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Introduction() {
+    return (
+        <section>
+            <div className='grid grid-cols 1 lg:grid-cols-12'>
+                <div className='col-span-7'>
+
+                    <h1 className='text-white text-5xl lg:text-6xl font-extrabold'>Hi, I'm Muhammad Aulia Rahman</h1>
+                    <p className='text-white text-lg lg:text-xl'>
+                        Passionate Programmer
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Introduction;
